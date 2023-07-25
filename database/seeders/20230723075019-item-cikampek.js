cb99c7ccc708c66ea1455f5aafe3c6ab04975092
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2023-07-25 15:09:20 +0700

    fix csv data

--- database/seeders/20230723075019-item-cikampek.js
+++ database/seeders/20230723075019-item-cikampek.js
@@ -30,17 +30,42 @@ module.exports = {
     //     branchId: 1
     //   },
     // ]);
-    const csvFilePath = path.join(__dirname, 'data', 'item.csv'); // Ubah sesuai dengan path file CSV Anda
-    const results = [];
+    // const csvFilePath = path.join(__dirname, '/../../data', 'book1.csv'); // Ubah sesuai dengan path file CSV Anda
+    // const results = [];
 
-    // Baca file CSV dan parse datanya
-    fs.createReadStream(csvFilePath)
-      .pipe(csvParser())
-      .on('data', (data) => results.push(data))
-      .on('end', async () => {
-        // Masukkan data ke dalam tabel menggunakan queryInterface.bulkInsert
-        await queryInterface.bulkInsert('Users', results, {});
-      });
+    // // Baca file CSV dan parse datanya
+    // fs.createReadStream(csvFilePath)
+    //   .pipe(csvParser())
+    //   .on('data', (data) => results.push(data))
+    //   .on('end', async () => {
+    //     // Masukkan data ke dalam tabel menggunakan queryInterface.bulkInsert
+    //     await queryInterface.bulkInsert('Items', results, {});
+    //     console.log(results)
+    //   });
+    const filePath = path.join(__dirname, '/../../data', 'cikampek.csv');
+    try {
+        const data = await fs.readFile(filePath); // Read the CSV file
+        const rows = data.toString().split('\n'); // Split CSV data into rows
+      console.log(rows)
+        for (const row of rows) {
+          const [id, type, color, stock, branchId] = row.split(','); // Assuming the CSV columns are name, email, and age
+          console.log(id, row)
+          // Insert data into the "Users" table
+          return await queryInterface.bulkInsert('items', [
+            {
+              id,
+              type,
+              color,
+              stock,
+              branchId,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          ]);
+        }
+      } catch (error) {
+        console.error('Error while seeding data:', error);
+      }
   },
 
   async down (queryInterface, Sequelize) {
---
+++ database/seeders/item.js
@@ -0,0 +1,38 @@
+const fs = require('fs-extra');
+const csvParser = require('csv-parser');
+const path = require('path');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const filePath = path.join(__dirname, '/../../data', 'cikampek.csv');
+    try {
+        const data = await fs.readFile(filePath); // Read the CSV file
+        const rows = data.toString().split('\n'); // Split CSV data into rows
+  
+        for (const row of rows) {
+          const [id, type, color, stock, branchId] = row.split(','); // Assuming the CSV columns are name, email, and age
+  
+          // Insert data into the "Users" table
+          await queryInterface.bulkInsert('Items', [
+            {
+                id: parseInt(id),
+                type,
+                color,
+                stock: parseInt(stock),
+                branchId: parseInt(branchId),
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          ]);
+        }
+      } catch (error) {
+        console.error('Error while seeding data:', error);
+      }
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Remove the seeded data
+    await queryInterface.bulkDelete('items', null, {});
+  },
+};
+
